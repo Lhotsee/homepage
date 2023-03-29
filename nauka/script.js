1093cd4edf35e4f73d2e83a72955e8b9b80995f1
Magda Ciszewska <magdaciszewska@outlook.com>
2023-03-29 20:52:08 +0200

    hidden section

--- nauka/script.js
+++ nauka/script.js
@@ -1,5 +1,16 @@
-let myNumber = 2
-
-let double = number => number * 2;
-
-console.log(double(myNumber))
+{
+    const hideSection = () => {
+      const section = document.querySelector(".js-section");
+      section.classList.add(".js-section--hidden");
+      section.style.display = "none";
+    };
+  
+    const init = () => {
+      const hideSectionButton = document.querySelector(".hide-section-button");
+      hideSectionButton.addEventListener("click", hideSection);
+    };
+  
+    init();
+  }
+  
+  console.log("cześć")
